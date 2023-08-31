11ec497383d0cc22c5b13e2c02e5a263259c6f03
KevinnZou <43654611+KevinnZou@users.noreply.github.com>
2023-08-31 14:44:04 +0800

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,37 @@
+rootProject.name = "MyApplication"
+
+include(":androidApp")
+include(":shared")
+include(":desktopApp")
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+        google()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+
+    plugins {
+        val kotlinVersion = extra["kotlin.version"] as String
+        val agpVersion = extra["agp.version"] as String
+        val composeVersion = extra["compose.version"] as String
+
+        kotlin("jvm").version(kotlinVersion)
+        kotlin("multiplatform").version(kotlinVersion)
+        kotlin("android").version(kotlinVersion)
+
+        id("com.android.application").version(agpVersion)
+        id("com.android.library").version(agpVersion)
+
+        id("org.jetbrains.compose").version(composeVersion)
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+        google()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+}
