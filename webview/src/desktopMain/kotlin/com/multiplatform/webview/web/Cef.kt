a3abced664cf502293326c7bbca6c066945f0334
DatLag <jeff.retz08@gmail.com>
2023-09-22 13:27:37 +0200

    added cef builder documentation removed test method

--- webview/src/desktopMain/kotlin/com/multiplatform/webview/web/Cef.kt
+++ webview/src/desktopMain/kotlin/com/multiplatform/webview/web/Cef.kt
@@ -1,9 +1,7 @@
 package com.multiplatform.webview.web
 
-import kotlinx.coroutines.InternalCoroutinesApi
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.internal.synchronized
 import kotlinx.coroutines.runBlocking
 import me.friwi.jcefmaven.CefAppBuilder
 import me.friwi.jcefmaven.EnumProgress
@@ -15,14 +13,6 @@ import org.cef.CefApp
 import org.cef.CefClient
 import org.cef.CefSettings
 import java.io.File
-
-suspend fun test() {
-    Cef.init(builder = {
-        settings {
-
-        }
-    })
-}
 
 data object Cef {
 
@@ -183,8 +173,20 @@ data object Cef {
         private var _args: Array<out String> = arrayOf()
         private var _settings: Settings = Settings()
 
+        /**
+         * If installation skipping is enabled, no checks against the installation directory will be performed and the download,
+         * installation and verification of the jcef natives has to be performed by the individual developer.
+         */
         var skipInstallation: Boolean = false
-        lateinit var installDir: File
+
+        /**
+         * Sets the install directory to use. Defaults to "./jcef-bundle".
+         */
+        var installDir: File = File("jcef-bundle")
+
+        /**
+         * Attach your own adapter to handle certain events in CEF.
+         */
         var appHandler: MavenCefAppHandlerAdapter? = null
 
         /**
@@ -199,14 +201,38 @@ data object Cef {
             _mirrors = mirror
         }
 
+        /**
+         * Add one or multiple arguments to pass to the JCef library.
+         * Arguments may contain spaces.
+         *
+         * Due to installation using maven some arguments may be overwritten
+         * again depending on your platform. Make sure to not specify arguments
+         * that break the installation process (e.g. subprocess path, resources path...)!
+         *
+         * @param args the arguments to add
+         */
         fun args(vararg args: String) = apply {
             _args = args
         }
 
+        /**
+         * Set [Settings] to change
+         * configuration parameters.
+         *
+         * Due to installation using maven some settings may be overwritten
+         * again depending on your platform.
+         */
         fun settings(settings: Settings) = apply {
             _settings = settings
         }
 
+        /**
+         * Retrieve the embedded [Settings] instance to change
+         * configuration parameters.
+         *
+         * Due to installation using maven some settings may be overwritten
+         * again depending on your platform.
+         */
         fun settings(builder: Settings.() -> Unit) = apply {
             settings(_settings.apply(builder))
         }
@@ -250,40 +276,192 @@ data object Cef {
     }
 
     data class Settings(
+        /**
+         * The location where cache data will be stored on disk. If empty an in-memory
+         * cache will be used for some features and a temporary disk cache for others.
+         * HTML5 databases such as localStorage will only persist across sessions if a
+         * cache path is specified.
+         */
         var cachePath: String? = null,
+
+        /**
+         * The path to a separate executable that will be launched for sub-processes.
+         * By default the browser process executable is used. See the comments on
+         * CefExecuteProcess() for details. Also configurable using the
+         * "browser-subprocess-path" command-line switch.
+         */
         var browserSubProcessPath: String? = null,
+
+        /**
+         * Set to true to disable configuration of browser process features using
+         * standard CEF and Chromium command-line arguments. Configuration can still
+         * be specified using CEF data structures or via the
+         * CefApp::OnBeforeCommandLineProcessing() method.
+         */
         var commandLineArgsDisabled: Boolean = false,
         var cookieableSchemesExcludeDefaults: Boolean = false,
         var cookieableSchemesList: String? = null,
+
+        /**
+         * Custom flags that will be used when initializing the V8 JavaScript engine.
+         * The consequences of using custom flags may not be well tested. Also
+         * configurable using the "js-flags" command-line switch.
+         */
         var javascriptFlags: String? = null,
+
+        /**
+         * The locale string that will be passed to Blink. If empty the default
+         * locale of "en-US" will be used. This value is ignored on Linux where locale
+         * is determined using environment variable parsing with the precedence order:
+         * LANGUAGE, LC_ALL, LC_MESSAGES and LANG. Also configurable using the "lang"
+         * command-line switch.
+         */
         var locale: String? = null,
+
+        /**
+         * The fully qualified path for the locales directory. If this value is empty
+         * the locales directory must be located in the module directory. This value
+         * is ignored on Mac OS X where pack files are always loaded from the app
+         * bundle Resources directory. Also configurable using the "locales-dir-path"
+         * command-line switch.
+         */
         var localesDirPath: String? = null,
+
+        /**
+         * The directory and file name to use for the debug log. If empty, the
+         * default name of "debug.log" will be used and the file will be written
+         * to the application directory. Also configurable using the "log-file"
+         * command-line switch.
+         */
         var logFile: String? = null,
+
+        /**
+         * The log severity. Only messages of this severity level or higher will be
+         * logged. Also configurable using the "log-severity" command-line switch with
+         * a value of "verbose", "info", "warning", "error", "error-report" or
+         * "disable".
+         */
         var logSeverity: LogSeverity = LogSeverity.Default,
+
+        /**
+         * Set to true to disable loading of pack files for resources and locales.
+         * A resource bundle handler must be provided for the browser and render
+         * processes via CefApp::GetResourceBundleHandler() if loading of pack files
+         * is disabled. Also configurable using the "disable-pack-loading" command-
+         * line switch.
+         */
         var packLoadingDisabled: Boolean = false,
+
+        /**
+         * To persist session cookies (cookies without an expiry date or validity
+         * interval) by default when using the global cookie manager set this value to
+         * true. Session cookies are generally intended to be transient and most Web
+         * browsers do not persist them. A |cache_path| value must also be specified to
+         * enable this feature. Also configurable using the "persist-session-cookies"
+         * command-line switch.
+         */
         var persistSessionCookies: Boolean = false,
+
+        /**
+         * Set to a value between 1024 and 65535 to enable remote debugging on the
+         * specified port. For example, if 8080 is specified the remote debugging URL
+         * will be http: *localhost:8080. CEF can be remotely debugged from any CEF or
+         * Chrome browser window. Also configurable using the "remote-debugging-port"
+         * command-line switch.
+         */
         var remoteDebuggingPort: Int = 0,
+
+        /**
+         * The fully qualified path for the resources directory. If this value is
+         * empty the cef.pak and/or devtools_resources.pak files must be located in
+         * the module directory on Windows/Linux or the app bundle Resources directory
+         * on Mac OS X. Also configurable using the "resources-dir-path" command-line
+         * switch.
+         */
         var resourcesDirPath: String? = null,
+
+        /**
+         * The number of stack trace frames to capture for uncaught exceptions.
+         * Specify a positive value to enable the CefV8ContextHandler::
+         * OnUncaughtException() callback. Specify 0 (default value) and
+         * OnUncaughtException() will not be called. Also configurable using the
+         * "uncaught-exception-stack-size" command-line switch.
+         */
         var uncaughtExceptionStackSize: Int = 0,
+
+        /**
+         * Value that will be returned as the User-Agent HTTP header. If empty the
+         * default User-Agent string will be used. Also configurable using the
+         * "user-agent" command-line switch.
+         */
         var userAgent: String? = null,
+
+        /**
+         * Value that will be inserted as the product portion of the default
+         * User-Agent string. If empty the Chromium product version will be used. If
+         * |userAgent| is specified this value will be ignored. Also configurable
+         * using the "user_agent_product" command-line switch.
+         */
         var userAgentProduct: String? = null,
+
+        /**
+         * Set to true to enable windowless (off-screen) rendering support. Do not
+         * enable this value if the application does not use windowless rendering as
+         * it may reduce rendering performance on some systems.
+         */
         var windowlessRenderingEnabled: Boolean = false
     ) {
 
+        /**
+         * The log severity. Only messages of this severity level or higher will be
+         * logged. Also configurable using the "log-severity" command-line switch with
+         * a value of "verbose", "info", "warning", "error", "error-report" or
+         * "disable".
+         */
         fun logSeverity(severity: CefSettings.LogSeverity) = apply {
             logSeverity = LogSeverity.fromJCefSeverity(severity)
         }
 
         /**
-         * This way we don't need to expose the jcef module
+         * Log severity levels.
+         *
+         * This way we don't need to expose the jcef module.
          */
         sealed interface LogSeverity {
+
+            /**
+             * Default logging (currently INFO logging).
+             */
             data object Default : LogSeverity
+
+            /**
+             * Verbose logging.
+             */
             data object Verbose : LogSeverity
+
+            /**
+             * INFO logging.
+             */
             data object Info : LogSeverity
+
+            /**
+             * WARNING logging.
+             */
             data object Warning : LogSeverity
+
+            /**
+             * ERROR logging.
+             */
             data object Error : LogSeverity
+
+            /**
+             * FATAL logging.
+             */
             data object Fatal : LogSeverity
+
+            /**
+             * Completely disable logging.
+             */
             data object Disable : LogSeverity
 
             fun toJCefSeverity(): CefSettings.LogSeverity = when (this) {
