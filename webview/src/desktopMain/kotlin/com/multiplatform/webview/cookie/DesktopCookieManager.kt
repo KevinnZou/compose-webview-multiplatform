5285387467c935b7c027a386f315fa743aef8356
邹铭杰 <zoumingjie17@163.com>
2023-09-28 10:48:53 +0800

    feat:desktop implementation of CookieManager.

--- webview/src/desktopMain/kotlin/com/multiplatform/webview/cookie/DesktopCookieManager.kt
+++ webview/src/desktopMain/kotlin/com/multiplatform/webview/cookie/DesktopCookieManager.kt
@@ -1,22 +1,81 @@
 package com.multiplatform.webview.cookie
 
-object DesktopCookieManager: CookieManager {
-    override fun setCookie(url: String, cookie: Cookie) {
-        TODO("Not yet implemented")
+import co.touchlab.kermit.Logger
+import org.cef.network.CefCookie
+import org.cef.network.CefCookieManager
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+import java.util.TimeZone
+
+object DesktopCookieManager : CookieManager {
+    /**
+     * CefCookieManager.getGlobalManager() is not available until CEF is initialized.
+     * Thus, we can only initialize it lazily.
+     */
+    private var desktopCookieManager: CefCookieManager? = null
+
+    override suspend fun setCookie(url: String, cookie: Cookie) {
+        if (desktopCookieManager == null) {
+            desktopCookieManager = CefCookieManager.getGlobalManager()
+        }
+        val currentTime = System.currentTimeMillis()
+        Logger.i { "DesktopCookieManager setCookie: $url, $cookie" }
+        desktopCookieManager!!.setCookie(
+            url, CefCookie(
+                cookie.name,
+                cookie.value,
+                cookie.domain,
+                cookie.path,
+                cookie.isSecure ?: false,
+                cookie.isHttpOnly ?: false,
+                Date(currentTime),
+                Date(currentTime),
+                Date(cookie.expiresDate ?: currentTime).before(Date(currentTime)),
+                Date(cookie.expiresDate ?: System.currentTimeMillis())
+            )
+        )
     }
 
-    override fun getCookies(url: String): List<Cookie> {
-        TODO("Not yet implemented")
+    override suspend fun getCookies(url: String): List<Cookie> {
+        if (desktopCookieManager == null) {
+            desktopCookieManager = CefCookieManager.getGlobalManager()
+        }
+        Logger.i { "DesktopCookieManager getCookies: $url" }
+        val cookieList = mutableListOf<Cookie>()
+        CefCookieManager.getGlobalManager().visitUrlCookies(
+            url, true
+        ) { cookie, _, _, _ ->
+            cookieList.add(
+                Cookie(
+                    name = cookie.name,
+                    value = cookie.value,
+                    domain = cookie.domain,
+                    path = cookie.path,
+                    expiresDate = cookie.expires?.time,
+                    sameSite = null,
+                    isSecure = cookie.secure,
+                    isHttpOnly = cookie.httponly,
+                    maxAge = null
+                )
+            )
+            true
+        }
+
+        return cookieList
     }
 
-    override fun removeAllCookies() {
-        TODO("Not yet implemented")
+    override suspend fun removeAllCookies() {
+        CefCookieManager.getGlobalManager().deleteCookies("", "")
     }
 }
 
 actual fun getCookieExpirationDate(expiresDate: Long): String {
-    TODO()
+    val sdf = SimpleDateFormat("EEE, dd MMM yyyy hh:mm:ss z", Locale.US).apply {
+        timeZone = TimeZone.getTimeZone("GMT")
+    }
+    return sdf.format(Date(expiresDate))
 }
 
 @Suppress("FunctionName") // Builder Function
-actual fun ActualCookieManager(): CookieManager = DesktopCookieManager
+actual fun WebViewCookieManager(): CookieManager = DesktopCookieManager
