f11cade0dd223e28a2e2e48aa99e7a07698c3c7e
邹铭杰 <zoumingjie17@163.com>
2023-11-29 20:03:23 +0800

    feat:Support Request Interception Desktop

--- webview/src/desktopMain/kotlin/com/multiplatform/webview/web/WebEngineExt.kt
+++ webview/src/desktopMain/kotlin/com/multiplatform/webview/web/WebEngineExt.kt
@@ -1,14 +1,21 @@
 package com.multiplatform.webview.web
 
+import com.multiplatform.webview.request.WebRequest
 import com.multiplatform.webview.util.KLogger
 import org.cef.CefSettings
 import org.cef.browser.CefBrowser
 import org.cef.browser.CefFrame
+import org.cef.callback.CefAuthCallback
+import org.cef.callback.CefCallback
 import org.cef.handler.CefDisplayHandler
 import org.cef.handler.CefLoadHandler
+import org.cef.handler.CefRequestHandler
+import org.cef.handler.CefResourceRequestHandler
+import org.cef.misc.BoolRef
 import org.cef.network.CefRequest
 import kotlin.math.abs
 import kotlin.math.ln
+import org.cef.security.CefSSLInfo
 
 /**
  * Created By Kevin Zou On 2023/9/12
@@ -151,6 +158,88 @@ internal fun CefBrowser.addLoadListener(
                         description = "Failed to load url: ${failedUrl}\n$errorText",
                     ),
                 )
+            }
+        },
+    )
+}
+
+internal fun CefBrowser.addRequestHandler(
+    state: WebViewState,
+    navigator: WebViewNavigator,
+) {
+    this.client.addRequestHandler(
+        object : CefRequestHandler {
+            override fun onBeforeBrowse(
+                browser: CefBrowser?,
+                frame: CefFrame?,
+                request: CefRequest?,
+                userGesture: Boolean,
+                isRedirect: Boolean,
+            ): Boolean {
+                val map = mutableMapOf<String, String>()
+                request?.getHeaderMap(map)
+                KLogger.d { "onBeforeBrowse ${request?.url} $map" }
+                val webRequest =
+                    WebRequest(
+                        request?.url.toString(),
+                        map,
+                    )
+                val intercept =
+                    navigator.requestInterceptor?.beforeRequest(
+                        webRequest,
+                        navigator,
+                    )
+                return intercept ?: false
+            }
+
+            override fun onOpenURLFromTab(
+                p0: CefBrowser?,
+                p1: CefFrame?,
+                p2: String?,
+                p3: Boolean,
+            ): Boolean {
+                return false
+            }
+
+            override fun getResourceRequestHandler(
+                p0: CefBrowser?,
+                p1: CefFrame?,
+                p2: CefRequest?,
+                p3: Boolean,
+                p4: Boolean,
+                p5: String?,
+                p6: BoolRef?,
+            ): CefResourceRequestHandler? {
+                return null
+            }
+
+            override fun getAuthCredentials(
+                p0: CefBrowser?,
+                p1: String?,
+                p2: Boolean,
+                p3: String?,
+                p4: Int,
+                p5: String?,
+                p6: String?,
+                p7: CefAuthCallback?,
+            ): Boolean {
+                return false
+            }
+
+            override fun onCertificateError(
+                p0: CefBrowser?,
+                p1: CefLoadHandler.ErrorCode?,
+                p2: String?,
+                p3: CefSSLInfo?,
+                p4: CefCallback?,
+            ): Boolean {
+                return false
+            }
+
+            override fun onRenderProcessTerminated(
+                p0: CefBrowser?,
+                p1: CefRequestHandler.TerminationStatus?,
+            ) {
             }
         },
     )
