cccef8e1b3da3abfa73523deb1da4383b5cd243b
邹铭杰 <zoumingjie17@163.com>
2023-11-29 19:50:46 +0800

    feat:Support Request Interception Structure Improve

--- webview/src/commonMain/kotlin/com/multiplatform/webview/request/RequestInterceptor.kt
+++ webview/src/commonMain/kotlin/com/multiplatform/webview/request/RequestInterceptor.kt
@@ -1,8 +1,13 @@
 package com.multiplatform.webview.request
 
+import com.multiplatform.webview.web.WebViewNavigator
+
 /**
  * Created By Kevin Zou On 2023/11/29
  */
 interface RequestInterceptor {
-    fun beforeRequest(request: WebRequest): Boolean
+    fun beforeRequest(
+        request: WebRequest,
+        navigator: WebViewNavigator,
+    ): Boolean
 }
