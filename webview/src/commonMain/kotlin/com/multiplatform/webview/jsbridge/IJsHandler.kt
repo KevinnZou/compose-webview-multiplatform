1c88c67328dabcf34c8fae55f0ba06c471838219
邹铭杰 <zoumingjie17@163.com>
2023-12-06 08:21:00 +0800

    feat:IJsHandler add processParams support

--- webview/src/commonMain/kotlin/com/multiplatform/webview/jsbridge/IJsHandler.kt
+++ webview/src/commonMain/kotlin/com/multiplatform/webview/jsbridge/IJsHandler.kt
@@ -1,5 +1,7 @@
 package com.multiplatform.webview.jsbridge
 
+import kotlinx.serialization.json.Json
+
 /**
  * Created By Kevin Zou On 2023/10/31
  */
@@ -12,4 +14,9 @@ interface IJsHandler {
         message: JsMessage,
         callback: (Any) -> Unit,
     )
+
+}
+
+inline fun <reified T : Any> IJsHandler.processParams(message: JsMessage): T {
+    return Json.decodeFromString(message.params)
 }
