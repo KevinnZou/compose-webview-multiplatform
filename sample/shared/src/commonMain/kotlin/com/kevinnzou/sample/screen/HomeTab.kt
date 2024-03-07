a78bc1e2fb21456697b0de5eb360c42c0f958eb6
邹铭杰 <zoumingjie17@163.com>
2024-03-07 15:43:48 +0800

    feat:Add test url

--- sample/shared/src/commonMain/kotlin/com/kevinnzou/sample/screen/HomeTab.kt
+++ sample/shared/src/commonMain/kotlin/com/kevinnzou/sample/screen/HomeTab.kt
@@ -1,10 +1,12 @@
 package com.kevinnzou.sample.screen
 
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import cafe.adriel.voyager.navigator.tab.Tab
 import cafe.adriel.voyager.navigator.tab.TabOptions
@@ -52,12 +54,14 @@ fun Home() {
         val bundle = webViewState.viewState
         if (bundle == null) {
             // This is the first time load, so load the home page.
-            navigator.loadUrl("https://github.com/KevinnZou/compose-webview-multiplatform")
+            // Test Url: https://en.wikipedia.org/wiki/Foo_Fighters
+            navigator.loadUrl("https://kotlinlang.org/docs/multiplatform.html")
         }
     }
 
     WebView(
         state = webViewState,
+        modifier = Modifier.fillMaxSize(),
         navigator = navigator,
         onDispose = {
             Logger.d(tag = "ComposeWebView") {
