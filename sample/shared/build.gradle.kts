f4d06c6823a0a017bbf47c15b1c1c334a12f47d1
邹铭杰 <zoumingjie17@163.com>
2024-03-24 09:11:55 +0800

    feat:Downgrade minSdk to 21

--- sample/shared/build.gradle.kts
+++ sample/shared/build.gradle.kts
@@ -76,7 +76,7 @@ android {
     sourceSets["main"].resources.srcDirs("src/commonMain/resources")
 
     defaultConfig {
-        minSdk = 24
+        minSdk = 21
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
