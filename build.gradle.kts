ba49ff3f8c62c9330ce65163ca56ea70aeedbb23
AppOutlet team <team.appoutlet@gmail.com>
2025-04-26 14:04:13 +0100

    chore: replace direct reference to the catalog reference

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,16 @@
 plugins {
     // this is necessary to avoid the plugins to be loaded multiple times
     // in each subproject's classloader
-    kotlin("multiplatform").apply(false)
-    kotlin("plugin.serialization").apply(false)
-    id("com.android.application").apply(false)
-    id("com.android.library").apply(false)
-    id("org.jetbrains.compose").apply(false)
-    id("org.jetbrains.dokka")
-    id("com.vanniktech.maven.publish") version "0.25.3" apply false
-    id("org.jlleitschuh.gradle.ktlint") version "11.6.1"
-    id("org.jetbrains.kotlin.plugin.atomicfu") version "1.9.20"
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.compose.multiplatorm) apply false
+    alias(libs.plugins.kotlin.multiplatform) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.maven.publish) apply false
+
+    alias(libs.plugins.dokka)
+    alias(libs.plugins.kotlin.atomicfu)
+    alias(libs.plugins.ktlint)
 }
 
 subprojects {
