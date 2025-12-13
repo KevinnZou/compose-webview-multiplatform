07dd86be3be23d8be03bbed55ec0a049ab4b919a
threethan <ethanjmedei@gmail.com>
2025-12-13 05:48:10 -0500

    (Desktop) Set UA via request handler instead of context: Fixes CookieManager

--- webview/src/desktopMain/kotlin/com/multiplatform/webview/web/CefRequestExt.kt
+++ webview/src/desktopMain/kotlin/com/multiplatform/webview/web/CefRequestExt.kt
@@ -4,23 +4,36 @@ import com.multiplatform.webview.setting.WebSettings
 import dev.datlag.kcef.KCEFResourceRequestHandler
 import org.cef.browser.CefBrowser
 import org.cef.browser.CefFrame
-import org.cef.browser.CefRequestContext
+import org.cef.handler.CefRequestHandler
+import org.cef.handler.CefRequestHandlerAdapter
+import org.cef.handler.CefResourceRequestHandler
+import org.cef.misc.BoolRef
 import org.cef.network.CefRequest
 
-internal fun createModifiedRequestContext(settings: WebSettings): CefRequestContext {
-    return CefRequestContext.createContext { browser, frame, request, isNavigation, isDownload, requestInitiator, disableDefaultHandling ->
-        object : KCEFResourceRequestHandler(
-            getGlobalDefaultHandler(browser, frame, request, isNavigation, isDownload, requestInitiator, disableDefaultHandling),
-        ) {
-            override fun onBeforeResourceLoad(
-                browser: CefBrowser?,
-                frame: CefFrame?,
-                request: CefRequest?,
-            ): Boolean {
-                if (request != null) {
-                    settings.customUserAgentString?.let(request::setUserAgentString)
+internal fun createModifiedRequestHandler(settings: WebSettings): CefRequestHandler {
+    return object : CefRequestHandlerAdapter() {
+        override fun getResourceRequestHandler(
+            browser: CefBrowser?,
+            frame: CefFrame?,
+            request: CefRequest?,
+            isNavigation: Boolean,
+            isDownload: Boolean,
+            requestInitiator: String?,
+            disableDefaultHandling: BoolRef?,
+        ): CefResourceRequestHandler {
+            return object : KCEFResourceRequestHandler(
+                getGlobalDefaultHandler(browser, frame, request, isNavigation, isDownload, requestInitiator, disableDefaultHandling),
+            ) {
+                override fun onBeforeResourceLoad(
+                    browser: CefBrowser?,
+                    frame: CefFrame?,
+                    request: CefRequest?,
+                ): Boolean {
+                    if (request != null) {
+                        settings.customUserAgentString?.let(request::setUserAgentString)
+                    }
+                    return super.onBeforeResourceLoad(browser, frame, request)
                 }
-                return super.onBeforeResourceLoad(browser, frame, request)
             }
         }
     }
