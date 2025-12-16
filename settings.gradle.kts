a713d210d521fbe0aeb82afb7c994aed62dcec25
threethan <ethanjmedei@gmail.com>
2025-12-16 00:19:35 -0500

    Update owner

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,6 +7,9 @@ pluginManagement {
         mavenCentral()
     }
 }
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
 
 dependencyResolutionManagement {
     repositories {
