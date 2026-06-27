14fea96861aaa3aaf8404f8ac3fdc769d0cf5641
Anders Borch <anders@cyborch.com>
2026-06-27 11:10:13 +0200

    Fixed delegate doc to say WKNavigationDelegateProtocol

--- webview/src/commonMain/kotlin/com/multiplatform/webview/web/WebView.kt
+++ webview/src/commonMain/kotlin/com/multiplatform/webview/web/WebView.kt
@@ -151,7 +151,7 @@ expect class WebViewFactoryParam
 /**
  * Platform specific parameters given to the WebView composable function:
  *   - On Android, this contains an optional `AccompanistWebViewClient` and `AccompanistWebChromeClient`
- *   - On iOS, this contains an optional `WKNavigationDelegate`
+ *   - On iOS, this contains an optional `WKNavigationDelegateProtocol`
  *   - On Desktop, this is currently unused
  */
 expect class PlatformWebViewParams
