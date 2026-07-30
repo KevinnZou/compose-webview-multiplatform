86108d0ecff0f4332a7607f89215d2dbecf75867
perezeral <perezeral@globalhitss.com>
2026-07-30 16:52:44 -0600

    [add][method] - Method for setup javascript code before download blod

--- webview/src/commonMain/kotlin/com/multiplatform/webview/download/DownloadInterceptor.kt
+++ webview/src/commonMain/kotlin/com/multiplatform/webview/download/DownloadInterceptor.kt
@@ -1,10 +1,19 @@
 package com.multiplatform.webview.download
 
 import com.multiplatform.webview.web.WebViewNavigator
+import kotlinx.serialization.json.JsonObject
 
 interface DownloadInterceptor {
-    fun onInterceptDownloadRequest(
+    fun onInterceptDownloadResponse(
         request: DownloadRequest,
         navigator: WebViewNavigator,
+        byteArray: ByteArray,
+        json: JsonObject?
     )
+
+    fun onRequiredOverrideJavascriptInterface(
+        request: DownloadRequest,
+        navigator: WebViewNavigator,
+        scriptCommand: String
+    ): String
 }
